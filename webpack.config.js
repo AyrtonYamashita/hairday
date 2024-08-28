c98ee3c05fa5ca045406d33cb8f80e0747b98563
Ayrton Yamashita <ayrtonyamashita@gmail.com>
2024-08-28 15:42:04 -0400

    Instalando o babel na aplicação

--- webpack.config.js
+++ webpack.config.js
@@ -33,13 +33,23 @@ module.exports = {
       }
     ]
   })
-],
+  ],
 
   module: {
     rules: [
       {
         test: /\.css/i,
         use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.js/i,
+        exclude: /node_module/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
       }
     ]
   }
