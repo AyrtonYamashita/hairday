80e5933da399ffb432f07167c832e27490ad6f7f
Ayrton Yamashita <ayrtonyamashita@gmail.com>
2024-08-29 10:18:15 -0400

    Buscando os agendamentos na api

--- src/modules/shedules/load.js
+++ src/modules/shedules/load.js
@@ -1,16 +1,20 @@
+import { getSchedulebyDay } from "../../services/schedule-fetch-by-day.js"
 import { hoursLoad } from "../form/hours-load.js"
 
 // Seleciona o input de data
 const selectedDate = document.getElementById("date")
 
-export function scheduleDays() {
+export async function scheduleDays() {
   // Obtém a data do input
   const date = selectedDate.value
+
+  // Busca na API os agendamentos para carregar no lado direito da tela.
+  const dailySchedules = await getSchedulebyDay({ date })
+  console.log(dailySchedules)
 
   // Renderiza as horas disponíveis.
   hoursLoad({ date })
 
-  // Busca na API os agendamentos para carregar no lado direito da tela.
 
   // Os horários disponíveis (horário futuro) do lado esquerdo (form)
 }
